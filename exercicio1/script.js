99be9df30475920e393f372dafc3d9d6521e2224
srheizenberg <cleitonfreelance@gmail.com>
2022-08-23 19:31:22 -0300

    exercicios-feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,33 @@
+/*
+exercicio 01 parte 01
+function name(nome) {
+    return `Olá, ${nome}`; 
+}
+*/
+
+/*
+exercicio 01 parte 02
+function tabuada(){
+    let numeroTabuada = 6
+    for(let contador = 1; contador <= 10; contador++) {
+        console.log(numeroTabuada + " x " + contador + " = " + (numeroTabuada * contador));
+    }
+  }
+*/
+
+/*
+expression de function
+exercicio 01 parte 03-1
+const name = function(nome){return `Olá, ${nome}`; 
+}
+*/
+/*
+Arrow Function exemplo
+exercicio 01 parte 03-2
+const tabuada = () => {
+    let numeroTabuada = 6
+    for(let contador = 1; contador <= 10; contador++) {
+        console.log(numeroTabuada + " x " + contador + " = " + (numeroTabuada * contador));
+    }
+  }
+*/
---
+++ exercicio2/script.js
@@ -0,0 +1,36 @@
+/*
+exercicio 02 parte 01
+function somar(n1, n2) {
+    console.log("A somar é", n1+n2);    
+}
+*/
+
+/*
+exercicio 02 parte 02
+function somar(n1, n2) {
+    if (n1 >= n2) {
+        console.log(`primeiro número é ${n1}\nentão ele é`,true)
+    }else{
+        console.log("else")
+    }    
+}
+*/
+
+/*
+exercicio 02 parte 03
+function numeroPar(n1) {
+    if (n1%2 === 0 ) {
+        console.log(`O número é Par`)
+    }else{
+        console.log("O número não é Par")
+    }    
+}
+*/
+
+/*
+exercicio 02 parte 04
+function nome(string) {
+    console.log([string.length]);
+    console.log([string.toLowerCase()]);    
+}
+*/
